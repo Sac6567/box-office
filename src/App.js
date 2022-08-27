7eb422557a36423ee55092868fa0aa61b4696fcf
Sac6567 <sac6567@gmail.com>
2022-08-27 13:32:29 +0530

    React router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <switch>
+      <Route exact={true} path="/">
+        This is home page
+      </Route>
+
+      <Route exact={true} path="/stared">
+        This is home page is stared
+      </Route>
+
+      <Route>404</Route>
+    </switch>
+  );
 }
 
 export default App;
