ec6e6c7bb0c5f35d4944df06951efbbbcc3cc59e
Sac6567 <sac6567@gmail.com>
2022-08-27 14:24:41 +0530

    Edit latout management & title

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <switch>
-        <Route exact={true} path="/">
-          <Home />
-        </Route>
+    <switch>
+      <Route exact={true} path="/">
+        <Home />
+      </Route>
 
-        <Route exact={true} path="/starred">
-          <Starred />
-        </Route>
+      <Route exact={true} path="/starred">
+        <Starred />
+      </Route>
 
-        <Route>
-          <div>404 Not found</div>
-        </Route>
-      </switch>
-    </div>
+      <Route>
+        <div>404 Not found</div>
+      </Route>
+    </switch>
   );
 }
 
