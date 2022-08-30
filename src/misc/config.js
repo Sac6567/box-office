0480eb988db8cb30cef65b420fc9eb0d7fb7f8b3
Sac6567 <sac6567@gmail.com>
2022-08-30 12:08:55 +0530

    finished start page

--- src/misc/config.js
+++ src/misc/config.js
@@ -1,12 +1,5 @@
 const API_BASE_URL = 'https://api.tvmaze.com';
 
-// fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-//       .then(r => r.json())
-//       .then(result => {
-//         setResults(result);
-//         console.log(result);
-//       });
-
 export async function apiGet(quarryString) {
   const response = await fetch(`${API_BASE_URL}${quarryString}`).then(r =>
     r.json()
